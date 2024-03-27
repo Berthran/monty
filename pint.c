60508fb610d7b2740e6646a195b075b1a7c64f24
Daniel Berthran <berthlearn@gmail.com>
2024-03-27 12:30:21 +0100

    create file to print the top of the stack

---
+++ pint.c
@@ -0,0 +1,23 @@
+/** CODE to implement printing the value at top of the stack */
+
+#include "monty.h"
+
+/**
+ * pall - prints the element at the top of the stack
+ * @stack: the stack data structure to print
+ * @line_number: the line number of the instruction in the MBCF
+ *
+ * Return: nothing
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		free_stack_t(stack);
+		fclose(file);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
