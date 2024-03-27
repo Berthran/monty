4ba793c25b226d7f5cdbf17465ab94b4783557d2
Daniel Berthran <berthlearn@gmail.com>
2024-03-27 11:41:20 +0100

    update header file

--- monty.h
+++ monty.h
@@ -1,22 +1,19 @@
-/* Header file containing function prototypes and struct definitions */
-
-/* Include guards */
 #ifndef MONTY_H
 #define MONTY_H
+
 
 /* Include header files */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <unistd.h>
 #include <fcntl.h>
 
 /* Data structures for stack and queue implementation */
 
-/**
- * 1.
- * DOUBLY-LINKED LIST FOR STACK OR QUEUE
- */
+/* 1. */
+/* DOUBLY-LINKED LIST FOR STACK OR QUEUE*/
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -34,10 +31,8 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
-/**
- * 2.
- * INSTRUCTION DATA TYPE FOR OPERATIONS ON THE STACK OR QUEUE
- */
+/* 2. */
+/* INSTRUCTION DATA TYPE FOR OPERATIONS ON THE STACK OR QUEUE */
 
 /**
  * struct instruction_s - opcode and its function
@@ -53,10 +48,14 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/* Interprete the instruction line */
+int lineInterpreter(stack_t **stack, char *line, int line_number);
+void (*get_func(char *opcode))(stack_t **, unsigned int);
 
 /* Handle instruction line with no arguments */
 int is_newline(char *inst_line);
-int is_emptyspaces(char *inst_line);
+int is_spaces(char *inst_line);
+int is_empty(char *inst_line);
 
 /* Functions to handle printing error messages */
 void print_errmsg_argfail(void);
@@ -64,4 +63,17 @@ void print_errmsg_openfail(char *argv[]);
 void print_errmsg_instfail(char *opcode, int line_n);
 void print_errmsg_mallocfail(void);
 
+
+/* Handle instruction line with arguments */
+char *get_opcode(char *inst_line);
+
+/* Push to Stack */
+int push(stack_t **stack, char *val, int line_number);
+void push_to_stack(stack_t **stack, int val);
+
+/* Print the Stack */
+void pall(stack_t **stack, unsigned int line_number);
+
+/* Clean up stack */
+void free_stack_t(stack_t **stack);
 #endif
