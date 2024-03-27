008dfe02edeb0ef733c95988942ee21cd9ac01dd
Daniel Berthran <berthlearn@gmail.com>
2024-03-27 12:41:50 +0100

    create file to remove element at top of stack

---
+++ pop.c
@@ -0,0 +1,28 @@
+/** CODE to implement removing the value at top of the stack */
+
+#include "monty.h"
+
+/**
+ * pop - removes the element at the top of the stack
+ * @stack: the stack data structure to print
+ * @line_number: the line number of the instruction in the MBCF
+ *
+ * Return: nothing
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop, stack empty\n", line_number);
+		free_stack_t(stack);
+		fclose(file);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	*stack = temp->next;
+	free(temp);
+}
