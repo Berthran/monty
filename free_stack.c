f966bd78adc96f74efc26cb442645d558705f016
Daniel Berthran <berthlearn@gmail.com>
2024-03-27 11:42:28 +0100

    create file to free a stack

---
+++ free_stack.c
@@ -0,0 +1,26 @@
+/* CODE to implement freeing a stack */
+
+#include "monty.h"
+
+/**
+ * free_stack_t - frees a doubly linked list representation of a stack
+ * @stack: the stack data structure
+ *
+ * return: nothing
+ */
+
+void free_stack_t(stack_t **stack)
+{
+	stack_t *temp;
+
+	if (stack != NULL)
+	{
+		while ((*stack)->next != NULL)
+		{
+			temp = *stack;
+			*stack = temp->next;
+			free(temp);
+		}
+		free(*stack);
+	}
+}
