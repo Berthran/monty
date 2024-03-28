576931e394ceaa5aef64815145588d9612c394b2
Daniel Berthran <berthlearn@gmail.com>
2024-03-28 02:18:29 +0100

    remove fail

--- pop.c
+++ pop.c
@@ -17,7 +17,6 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop, stack empty\n", line_number);
-		printf("pop fail\n");
 		free_stack_t(stack);
 		fclose(file);
 		exit(EXIT_FAILURE);
@@ -26,5 +25,4 @@ void pop(stack_t **stack, unsigned int line_number)
 	temp = *stack;
 	*stack = temp->next;
 	free(temp);
-	printf("fail pop\n");
 }
--- push.c
+++ push.c
@@ -33,6 +33,5 @@ void push(stack_t **stack, int val)
 		}
 		/* Move stack to the top */
 		*stack = new_block;
-		printf("fail push\n");
 	}
 }
