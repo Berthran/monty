43d3e58eceacdeb12f8c547d339ca127fb6d6939
Daniel Berthran <berthlearn@gmail.com>
2024-03-28 15:28:12 +0100

    change fprintf to dprintf

--- print_opcode_errmsg.c
+++ print_opcode_errmsg.c
@@ -12,7 +12,7 @@
 
 void print_errmsg_pushfail(stack_t **stack, int line_number)
 {
-	fprintf(stderr, "L%d: usage: push integer\n", line_number);
+	dprintf(STDERR_FILENO, "L%d: usage: push integer\n", line_number);
 	free_stack_t(stack);
 	fclose(file);
 	exit(EXIT_FAILURE);
@@ -29,7 +29,7 @@ void print_errmsg_pushfail(stack_t **stack, int line_number)
 
 void print_errmsg_opcodefail(stack_t **stack, int line_number, char *opcode)
 {
-	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+	dprintf(STDERR_FILENO, "L%d: unknown instruction %s\n", line_number, opcode);
 	free_stack_t(stack);
 	fclose(file);
 	exit(EXIT_FAILURE);
