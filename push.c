f4342c18b338c5a66a3d735e8327592d9d8e99fd
Daniel Berthran <berthlearn@gmail.com>
2024-03-29 06:53:23 +0100

    push function with the given prototype

--- push.c
+++ push.c
@@ -5,23 +5,31 @@
 /**
  * push - adds a value to the top of the stack
  * @stack: the stack data structure to push to
- * @val: tha value to push to the stack
- *
+ * @line_number: the line number of the instruction in the MBCF
  * Return: nothing
  */
 
-void push(stack_t **stack, int val)
+void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_block = NULL;
+	char *val;
+	int n;
 
 	if (stack != NULL)
 	{
-		/* Create a new block of memory */
+		/* Attempt to get the value */
+		val = strtok(NULL, " ");
+		if (val == NULL || (atoi(val) == 0 && *val != '0'))
+			print_errmsg_pushfail(stack, line_number);
+
+		/* If value was gotten, create a new block of memory */
 		new_block = (stack_t *)malloc(sizeof(stack_t));
 		if (new_block == NULL)
 			print_errmsg_mallocfail();
+
 		/* Assign values to the members of the new block */
-		new_block->n = val;
+		n = atoi(val);
+		new_block->n = n;
 		new_block->prev = NULL;
 		new_block->next = NULL;
 
